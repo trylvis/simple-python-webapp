d078011dcf0e6370290d02c4d88101d187e352e1
Tarjei Ylvisåker / Intility AS <tarjei.ylvisaker@intility.no>
2019-02-12 13:13:15 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+@app.route("/")
+def main():
+    return "Welcome!"
+
+@app.route('/how are you')
+def hello():
+    return 'I am good, how about you?'
+
+if __name__ == "__main__":
+    app.run()
