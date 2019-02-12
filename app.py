425fec481b2148849723d67e7da8c2e97c0d6939
trylvis <trylvis@outlook.com>
2019-02-12 14:04:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+app = Flask(__name__)
 
 @app.route("/")
 def main():
@@ -10,4 +11,4 @@ def hello():
     return 'I am good, how about you?'
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=8080)
